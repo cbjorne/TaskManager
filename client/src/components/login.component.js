711bdee0d547cdeaed1d4291caeef2ba4888df8a
Colin <cbjorne@nmu.edu>
2024-03-22 13:18:50 -0400

    Convert login.component.js to functional

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 const CryptoJS = require('crypto-js');
 
@@ -6,89 +6,64 @@ const encrypt = pw => {
     return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(pw));
 }
 
-export default class Login extends Component {
-    constructor(props){
-        super(props);
+const Login = (props) => {
+    const [allValues, setAllValues] = useState({
+        requirements: '',
+        username: '',
+        password: ''
+    });
 
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onRegister = this.onRegister.bind(this);
+    const changeHandler = (e) => {
+        setAllValues({ ...allValues, [e.target.name]: e.target.value });
+    };
 
-        this.state = {
-            requirements: '',
-            username: '',
-            password: '',
-        }
-    }
+    const register = (e) => {
+        e.preventDefault();
+        window.location = '/register';
+    };
 
-    onChangeUsername(e) {
-        this.setState({
-            username: e.target.value
-        });
-    }
-
-    onChangePassword(e) {
-        this.setState({
-            password: e.target.value
-        });
-    }
-
-    onRegister(e) {
-        e.preventDefault()
-        
-        window.location = '/register'
-    }
-
-    onSubmit(e) {
+    const submit = (e) => {
         e.preventDefault();
 
         const user = {
-            username: this.state.username,
-            password: encrypt(this.state.password)
+            username: allValues.username,
+            password: encrypt(allValues.password)
         };
 
         axios.post('http://localhost:5000/login', user)
-        .then(res => {
-            if(res.data.status){
-                this.setState({
-                    requirements: '',
-                    username: '',
-                    password: ''
-                });
+            .then(res => {
+                if (res.data.status) {
+                    window.location = `${res.data.token}/projects`;
+                }
+                else {
+                    allValues.requirements = res.data;
+                    setAllValues({ ...allValues });
+                }
+            })
+            .catch(err => console.log(err));
+    };
 
-                window.location = `${res.data.token}/projects`;
-            }
-            else{
-                this.setState({
-                    requirements: res.data
-                });
-            }
-        })
-        .catch(err => console.log(err));
-    }
+    return (
+        <div>
+            <h2>Login</h2>
+            <h4>{allValues.requirements}</h4>
+            <form onSubmit={submit}>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" required className="form-control" name="username" value={allValues.name} onChange={changeHandler} />
+                </div>
+                <div className="form-group">
+                    <label>Password </label>
+                    <input type="password" required className="form-control" name="password" value={allValues.password} onChange={changeHandler} />
+                </div>
+                <br />
+                <div className="form-group">
+                    <input onClick={register} type="button" value="Register" className="btn btn-primary" />
+                    <input type="submit" value="Submit" className="btn btn-primary" />
+                </div>
+            </form>
+        </div>
+    );
+};
 
-    render() {
-        return (
-            <div>
-                <h2>Login</h2>
-                <h4>{this.state.requirements}</h4>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Username</label>
-                        <input type="text" required className="form-control" value={this.state.name} onChange={this.onChangeUsername}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Password </label>
-                        <input type="password" required className="form-control" value={this.state.password} onChange={this.onChangePassword}/>
-                    </div>
-                    <br/>
-                    <div className="form-group">
-                        <input onClick={this.onRegister} type="button" value="Register" className="btn btn-primary"/>
-                        <input type="submit" value="Submit" className="btn btn-primary"/>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
+export default Login;
