04e8bfc0fc6225f5e0b81cd1c5bb351a9dc69cb1
Colin <cbjorne@nmu.edu>
2024-03-21 23:12:30 -0400

    Remove legacy code from delete-user.component.js

--- client/src/components/delete-user.component.js
+++ client/src/components/delete-user.component.js
@@ -43,7 +43,7 @@ const DeleteUser = (props) => {
                     }
                 });
         }
-    }
+    };
 
     return (
         <div>
@@ -64,86 +64,3 @@ const DeleteUser = (props) => {
 };
 
 export default DeleteUser;
-
-
-
-
-/*const withRouter = WrappedComponent => props => {
-    const params = useParams();
-
-    return (
-        <WrappedComponent params={params}/>
-    );
-}
-
-class DeleteUser extends Component {
-    constructor(props) {
-        super(props);
-
-        this.onConfirmUsername = this.onConfirmUsername.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            username: '',
-            input: ''
-        }
-    }
-
-    componentDidMount() {
-        axios.get(`http://localhost:5000/${this.props.params.token}/user/${this.props.params.id}`)
-        .then(res => {
-            if(!res.data.status){
-                window.location = '/login';
-            }
-            else{
-                this.setState({
-                    username: res.data.user.username
-                });
-            }
-        })
-        .catch(err => console.log(err));
-    }
-
-    onConfirmUsername(e) {
-        this.setState({
-            input: e.target.value
-        })
-    }
-
-    onSubmit(e) {
-        e.preventDefault()
-
-        if(this.state.input === this.state.username) {
-            axios.delete(`http://localhost:5000/${this.props.params.token}/user/delete/${this.props.params.id}`)
-            .then(res => {
-                if(res.data.status) {
-                    window.location = `/${this.props.params.token}/users`;
-                }
-                else{
-                    window.location = '/register';
-                }
-            });
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <Navbar token={this.props.params.token}/>
-                <br/>
-                <h3>Delete {this.state.username}?</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Confirm Username</label>
-                        <input type="text" required className="form-control" value={this.state.input} onChange={this.onConfirmUsername}/>
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Delete User" className="btn btn-primary"/>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
-
-export default withRouter(DeleteUser);*/
