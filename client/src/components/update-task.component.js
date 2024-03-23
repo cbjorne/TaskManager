581de1b71b34ed1272fa8b4a55992e99b1d2724a
Colin <cbjorne@nmu.edu>
2024-03-23 00:59:26 -0400

    Removed legacy imports and code

--- client/src/components/update-task.component.js
+++ client/src/components/update-task.component.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, Component } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
